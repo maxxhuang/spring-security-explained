c11761f5e48f1737b8f4ebebf406e539fccad60b
maxx <maxxlife@gmail.com>
2016-04-20 01:28:48 +0800

    draft layout, initial idea

---
+++ src/main/java/Chapter00_Security_Framework_Overview.java
@@ -0,0 +1,18 @@
+/**
+ *
+ */
+public class Chapter00_Security_Framework_Overview {
+
+    /**
+     *
+     * <ol>
+     *     <li>Gather info (e.g. username / password) for Authentication</li>
+     *     <li>Configure authentication / authorization sources</li>
+     *     <li>Specify </li>
+     * </ol>
+     */
+    public void section00_What_Security_Framework_Does_For_You() {
+
+    }
+
+}
---
+++ src/main/java/Chapter01_Architecture/Authentication_and_SecurityContext.java
@@ -0,0 +1,24 @@
+package Chapter01_Architecture;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.Authentication;
+
+/**
+ *
+ * <b>Authentication</b>
+ * <ol>
+ *     <li>the token for an authentication request</li>
+ *     <li>
+ *         or an authenticated principal once the request has been processed by the
+ *         {@link AuthenticationManager#authenticate(Authentication)}
+ *     </li>
+ * </ol>
+ *
+ */
+public class Authentication_and_SecurityContext {
+
+    public static void main(String[] args) {
+
+    }
+
+}
